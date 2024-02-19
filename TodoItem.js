19aabe10779664e9025ec409e40eb717c93ac35a
Layomi1 <107419093+Layomi1@users.noreply.github.com>
2024-02-19 13:16:25 +0100

    adding the tick to input on click

--- TodoItem.js
+++ TodoItem.js
@@ -1,47 +1,64 @@
 class TodoList extends HTMLElement {
   constructor() {
     super();
-    const shadow = this.attachShadow({ mode: "open" });
-    const isChecked = this.getAttribute("checked");
-    const isLastChecked = this.hasAttribute("last-checked");
+    this.shadow = this.attachShadow({ mode: "open" });
 
-    shadow.innerHTML = `
+    this.shadow.innerHTML = `
     <style>
     label {
+      
+      display: flex;
+      font-size: 14px;
+      position: relative;
+      gap: 8px;
+    }
+     slot{
       color: #C6C6C6;
-        display: block;
-        font-size: 14px;
-       
-    }
-    
+      cursor:pointer;
+     }
+
     input[type="checkbox"] {
-      outline: none;
-      border: 2px solid green;
-      border-radius: 50% !important;
-      color: red;
-      width: 15px;
-      height: 15px;
-
+      cursor:pointer;
+      position:absolute;
+      opacity:0;
     }
     
-    input[type="checkbox"]:checked {
-      background-color: green !important;
+    label slot::before {
+      content:'';
+      width:1em;
+      height:1em;
+      display: flex;
+      color:black;
+      align-items: center;
+      justify-content: center;
+      border-radius: 50%;
+      border: 1px solid #B5EA81;
+      
     }
 
+    label slot:hover::before,
+    input[type='checkbox']:hover + label slot::before{
+      background-color: green;
+     }
+
+     label input[type='checkbox']:checked + label slot::before {
+      content:'\\2714';
+      color: white;
+      background-color: #B5EA81;
+      display: flex;
+      align-items:center;
+      justify-content:center;
+    
+     }
+     
     </style>
 
       <label>
-        <input type="checkbox" $(isChecked ? 'checked' : '') />
+        <input type="checkbox" />
         <slot></slot>
       </label>
     
     `;
-
-    if (isLastChecked) {
-      const input = shadow.querySelector('input[type="checkbox"]');
-      input.checked = true;
-      input.disabled = true;
-    }
   }
 }
 customElements.define("todo-list", TodoList);
